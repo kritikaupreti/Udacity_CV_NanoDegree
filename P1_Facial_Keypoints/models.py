09fef9ef6a5a961cf051cc1378db37d60aaabd77
Ubuntu <ubuntu@ip-172-31-28-236.us-east-2.compute.internal>
2018-12-03 02:15:52 +0000

    Project 1 - Facial Keypoints Detection

---
+++ P1_Facial_Keypoints/models.py
@@ -0,0 +1,57 @@
+## TODO: define the convolutional neural network architecture
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# can use the below import should you choose to initialize the weights of your Net
+import torch.nn.init as I
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        ## output size = (W-F)/S +1 = (224-5)/1 +1 = 220
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 5)  
+        ## output size = (W-F)/S +1 = (110-5)/1 +1 = 106
+        
+        # 64 outputs * the 53*53 filtered/pooled map size
+        self.fc1 = nn.Linear(64*53*53, 1000)
+        
+        # dropout with p=0.4
+        self.fc1_drop = nn.Dropout(p=0.4)
+        
+        # finally, create 10 output channels (for the 10 classes)
+        self.fc2 = nn.Linear(1000, 68*2)
+        
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        ## x = self.pool(F.relu(self.conv1(x)))
+        # two conv/relu + pool layers
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        # prep for linear layer
+        # this line of code is the equivalent of Flatten in Keras
+        x = x.view(x.size(0), -1)
+        
+        # two linear layers with dropout in between
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = self.fc2(x)
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
